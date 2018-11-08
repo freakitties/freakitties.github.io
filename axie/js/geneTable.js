03156bc3e8899c5dd745ff00e112267e6c7e4b61
freak <freak>
2018-11-08 01:42:54 -0800

    revert last change as it doesn't work well

--- axie/js/geneTable.js
+++ axie/js/geneTable.js
@@ -596,28 +596,6 @@
                 }
             });
 
-            //filter on enter instead of per keystroke
-            jQuery.fn.dataTableExt.oApi.fnFilterOnReturn = function (oSettings) {
-                var _that = this;
-
-                this.each(function (i) {
-                    $.fn.dataTableExt.iApiIndex = i;
-                    var $this = this;
-                    var anControl = $('input', _that.fnSettings().aanFeatures.f);
-                    anControl
-                        .unbind('keyup search input')
-                        .bind('keypress', function (e) {
-                            if (e.which == 13) {
-                                $.fn.dataTableExt.iApiIndex = i;
-                                _that.fnFilter(anControl.val());
-                            }
-                        });
-                    return this;
-                });
-                return this;
-            };
-            $('.dataTable').dataTable().fnFilterOnReturn();
-
             $("#axieTable_length").after('&nbsp; <input id="binaryToggle" class="text-right ml-4" type="checkbox" onclick="toggleBinary()"/> Hide binary');
 
             //doesn't copy nicely yet. don't show for now
