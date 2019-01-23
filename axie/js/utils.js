755c32eb52a62f510e4837fb624106181de07653
freak <freak>
2019-01-22 19:10:12 -0800

    getqueryparameters

--- axie/js/utils.js
+++ axie/js/utils.js
@@ -15,3 +15,15 @@
     function weakEscape(str) {
         return str.replace(/'/g, "&apos;").replace(/"/g, "&quot;").replace(/</g, '&lt;').replace(/>/g, "&gt;");
     }
+
+    function getQueryParameter(name) {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] == name) {
+                return pair[1];
+            }
+        }
+        return false;
+    }
