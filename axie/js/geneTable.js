51e09c3f1d578d0fe0cac7a85f762ac1fc944339
freak <freak>
2019-02-12 18:23:01 -0800

    clean up/refactor

--- axie/js/geneTable.js
+++ axie/js/geneTable.js
@@ -4,14 +4,7 @@
     const classGeneMap = {"0000": "beast", "0001": "bug", "0010": "bird", "0011": "plant", "0100": "aquatic", "0101": "reptile", "1000": "??? Nut", "1001": "??? Star", "1010": "??? Moon"};
     const typeOrder = {"eyes": 2, "ears": 4, "mouth": 3, "horn": 5, "back": 6, "tail": 7};  //swap ears and mouth ordering
     const regionGeneMap = {"00000": "global", "00001": "japan"};
-    const tagGeneMap ={"00000": "Normal", "00001": "Origin", "00010": "Agamogenesis", "00011": "Meo Corp", "00100": "Meo Corp II"}
-
-    const expCheckpointABI = [{"constant":false,"inputs":[],"name":"unpause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"account","type":"address"}],"name":"isPauser","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"dailyExpLimit","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renouncePauser","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"coreExtraContract","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renounceOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"account","type":"address"}],"name":"addPauser","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"isOwner","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"account","type":"address"}],"name":"addMinter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"renounceMinter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"account","type":"address"}],"name":"isMinter","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"coreContract","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_coreContract","type":"address"},{"name":"_coreExtraContract","type":"address"},{"name":"_dailyExpLimit","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_axieId","type":"uint256"},{"indexed":false,"name":"_exp","type":"uint256"}],"name":"ExpCheckpoint","type":"event"},{"anonymous":false,"inputs":[],"name":"Paused","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpaused","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"account","type":"address"}],"name":"PauserAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"account","type":"address"}],"name":"PauserRemoved","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"}],"name":"OwnershipRenounced","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"account","type":"address"}],"name":"MinterAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"account","type":"address"}],"name":"MinterRemoved","type":"event"},{"constant":true,"inputs":[{"name":"_axieId","type":"uint256"}],"name":"getCheckpoint","outputs":[{"name":"_exp","type":"uint256"},{"name":"_createdAt","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_axieId","type":"uint256"},{"name":"_exp","type":"uint256"},{"name":"_createdAt","type":"uint256"},{"name":"_signature","type":"bytes"},{"name":"_subscriber","type":"address"},{"name":"_data","type":"bytes"}],"name":"checkpointAndCall","outputs":[{"name":"_axieExp","type":"uint256"}],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"_axieIds","type":"uint256[]"},{"name":"_expList","type":"uint256[]"},{"name":"_createdAtList","type":"uint256[]"},{"name":"_signatures","type":"bytes"},{"name":"_subscriber","type":"address"},{"name":"_data","type":"bytes"}],"name":"checkpointForMultiAndCall","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"_axieId","type":"uint256"},{"name":"_exp","type":"uint256"},{"name":"_createdAt","type":"uint256"},{"name":"_signature","type":"bytes"}],"name":"checkpoint","outputs":[{"name":"_axieExp","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_axieIds","type":"uint256[]"},{"name":"_expList","type":"uint256[]"},{"name":"_createdAtList","type":"uint256[]"},{"name":"_signatures","type":"bytes"}],"name":"checkpointForMulti","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_dailyExpLimit","type":"uint256"}],"name":"setDailyExpLimit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}];
-    const expCheckpointAddress = "0x71FfC95Ca3BcEbF26024f689F40006182916167f";
-    var web3query = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/mew'));
-    var expCheckpointContract = web3query.eth.contract(expCheckpointABI);
-    var expCheckpointInstance = expCheckpointContract.at(expCheckpointAddress);
-
+    const tagGeneMap ={"00000": "Normal", "00001": "Origin", "00010": "Agamogenesis", "00011": "Meo Corp", "00100": "Meo Corp II"};
     var hash = window.location.hash.substr(1);
 
     function getClassFromGenes(id) {
@@ -659,23 +652,12 @@
     }
 
     function setExtraData(id) {
+        let axie = axies[id];
         if (!("breedCount" in axies[id])) {
-                $.get("https://api.axieinfinity.com/v1/axies/" + id, function(data) {
-                    expCheckpointInstance.getCheckpoint(id, (error, result) => {
-                        if (error) {
-                            console.log("Error getting Exp Checkpoint from the contract");
-                            return;
-                        }
-                        axies[id].breedCount = data.breedCount;
-                        axies[id].pendingExp = data.pendingExp ? data.pendingExp : 0;
-                        axies[id].totalSynced = result[0].toNumber();
-                        axies[id].truePending = axies[id].pendingExp - axies[id].totalSynced;
-                        $("#extraData_" + id).text("Exp: " + axies[id].exp + ", Pending exp: " + axies[id].pendingExp + ", Breeds: " + axies[id].breedCount);
-                    });
-                }).fail(() => {
-                    console.log("Failed to get axie details: " + axieID);
-                });
+            getExtendedAxieData(axie, () => {
+                $("#extraData_" + axie.id).text("Exp: " + axie.exp + ", Pending exp: " + axie.pendingExp + ", Breeds: " + axie.breedCount);
+            });
         } else {
-            $("#extraData_" + id).text("Exp: " + axies[id].exp + ", Pending exp: " + axies[id].pendingExp + ", Breeds: " + axies[id].breedCount);
+            $("#extraData_" + axie.id).text("Exp: " + axie.exp + ", Pending exp: " + axie.pendingExp + ", Breeds: " + axie.breedCount);
         }
     }
