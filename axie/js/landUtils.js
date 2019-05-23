b2c3e106c4faa7c1d2791d4c8b609dd3a78ffac3
freak <freak>
2019-05-23 15:27:08 -0700

    land sales map and land market page

---
+++ axie/js/landUtils.js
@@ -0,0 +1,16 @@
+async function getItems() {
+    let rarity = "";  //mystic, epic, rare, common, empty (land)
+    let sort = "lowest_price"; //lowest_price, recently_listed, highest_price
+    let baseUrl = "https://axieinfinity.com/marketplace-api/query-assets?sorting=" + sort + "&rarity=" + rarity + "&offset=0&count=";
+    let r = await fetch(baseUrl + "10");
+    data = await r.json();
+    let url = baseUrl + parseInt(data.total);
+    r = await fetch(url);
+    data = await r.json();
+    items = data.results;
+    for (let i in items) {
+        //if (items[i].assetType != "land") continue;
+        id = items[i].realTokenId;
+        allItems[id] = items[i];
+    }
+}
