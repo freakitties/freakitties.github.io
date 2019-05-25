90b48064073ec63700cb22f7e1d37f7983a055f1
freak <freak>
2019-05-25 13:01:13 -0700

    updated for api change

--- axie/js/landUtils.js
+++ axie/js/landUtils.js
@@ -1,7 +1,8 @@
 async function getItems() {
     let rarity = "";  //mystic, epic, rare, common, empty (land)
     let sort = "lowest_price"; //lowest_price, recently_listed, highest_price
-    let baseUrl = "https://axieinfinity.com/marketplace-api/query-assets?sorting=" + sort + "&rarity=" + rarity + "&offset=0&count=";
+    //let baseUrl = "https://axieinfinity.com/marketplace-api/query-assets?sorting=" + sort + "&rarity=" + rarity + "&offset=0&count=";
+    let baseUrl = "https://axieinfinity.com/marketplace-api/query-assets?sorting=" + sort + "&item_name=land&offset=0&count=";
     let r = await fetch(baseUrl + "10");
     data = await r.json();
     let url = baseUrl + parseInt(data.total);
