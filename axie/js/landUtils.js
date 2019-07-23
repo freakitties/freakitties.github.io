8eabf91e1dc71f9add38fd0b994dfd79a87b360a
freak <freak>
2019-07-23 01:43:59 -0700

    fixed missing sale items

--- axie/js/landUtils.js
+++ axie/js/landUtils.js
@@ -2,16 +2,22 @@ async function getItems() {
     let rarity = "";  //mystic, epic, rare, common, empty (land)
     let sort = "lowest_price"; //lowest_price, recently_listed, highest_price
     //let baseUrl = "https://axieinfinity.com/marketplace-api/query-assets?sorting=" + sort + "&rarity=" + rarity + "&offset=0&count=";
-    let baseUrl = "https://axieinfinity.com/marketplace-api/query-assets?sorting=" + sort + "&item_name=land&offset=0&count=";
-    let r = await fetch(baseUrl + "10");
+    let baseUrl = "https://axieinfinity.com/marketplace-api/query-assets?sorting=" + sort + "&item_name=land&count=";
+    let r = await fetch(baseUrl + "1");
     data = await r.json();
-    let url = baseUrl + parseInt(data.total);
-    r = await fetch(url);
-    data = await r.json();
-    items = data.results;
-    for (let i in items) {
-        //if (items[i].assetType != "land") continue;
-        id = items[i].realTokenId;
-        allItems[id] = items[i];
+
+    let total = parseInt(data.total);
+    let url = baseUrl + total + "&offset=";
+    for (let offset = 0; offset < total; offset += 500) {
+        url = baseUrl + total + "&offset=" + offset;
+        r = await fetch(url);
+        data = await r.json();
+        items = data.results;
+        for (let i in items) {
+            //if (items[i].assetType != "land") continue;
+            id = items[i].realTokenId;
+            allItems[id] = items[i];
+        }
+
     }
 }
