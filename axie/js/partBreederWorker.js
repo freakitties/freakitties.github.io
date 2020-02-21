f9f13f60415531e1b5f0d283a10dce72d209b2a0
freak <freak>
2020-02-20 22:53:17 -0800

    make part breeder use global part name, but include japanese/xmas parts in breeding candidates

--- axie/js/partBreederWorker.js
+++ axie/js/partBreederWorker.js
@@ -1,12 +1,44 @@
 onmessage = function(e) {
     if ("selectedParts" in e.data) {
-        offThreadCalc(e.data.selectedParts, e.data.axies, e.data.ignoreList);
+        offThreadCalc(e.data.selectedParts, e.data.axies, e.data.ignoreList, e.data.traitMapping);
     }
 }
 
-function offThreadCalc(selectedParts, axies, ignoreList) {
+//hack replace skinned
+var skinned = {eyes: {}, ears: {}, mouth: {}, horn: {}, back: {}, tail: {}};
+function replaceSkinned(traitMapping, axies) {
+    for (let cls in traitMapping) {
+        for (let partType in traitMapping[cls]) {
+            for (let binary in traitMapping[cls][partType]) {
+                //["japan","xmas"].some(r=>Object.keys(traitMapping["beast"].horn["001000"]).includes(r))
+                if ("japan" in traitMapping[cls][partType][binary]) {
+                    let japanId = partType.toLowerCase() + "-" + traitMapping[cls][partType][binary].japan.toLowerCase().replace(/\s/g, "-").replace(/[\?']/g, "");
+                    let globalId = partType.toLowerCase() + "-" + traitMapping[cls][partType][binary].global.toLowerCase().replace(/\s/g, "-").replace(/[\?']/g, "");
+                    skinned[partType][japanId] = globalId;
+                }
+            }
+        }
+    }
+    for (let id in axies) {
+        for (let partType in skinned) {
+            if (axies[id].traits[partType].d.partId in skinned[partType]) {
+                axies[id].traits[partType].d.partId = skinned[partType][axies[id].traits[partType].d.partId];
+            }
+            if (axies[id].traits[partType].r1.partId in skinned[partType]) {
+                axies[id].traits[partType].r1.partId = skinned[partType][axies[id].traits[partType].r1.partId];
+            }
+            if (axies[id].traits[partType].r2.partId in skinned[partType]) {
+                axies[id].traits[partType].r2.partId = skinned[partType][axies[id].traits[partType].r2.partId];
+            }
+        }
+    }
+}
+
+function offThreadCalc(selectedParts, axies, ignoreList, traitMapping) {
     let selectedIds = Object.keys(selectedParts);
     console.log("sub " + selectedIds);
+
+    replaceSkinned(traitMapping, axies);
 
     let typePartMap = {};
     for (let id in selectedParts) {
@@ -14,15 +46,34 @@ function offThreadCalc(selectedParts, axies, ignoreList) {
     }
 
     var matchingParts = {};
+    let numMatched = 0;
     for (let id in axies) {
         if (ignoreList.has(parseInt(id))) continue;
         let axie = axies[id];
         for (let typ in typePartMap) {
             if (selectedIds.includes(axie.traits[typ].d.partId) || selectedIds.includes(axie.traits[typ].r1.partId) || selectedIds.includes(axie.traits[typ].r2.partId)) {
                 matchingParts[axie.id] = axie;
+                numMatched++;
                 break;
             }
         }
+    }
+    if (numMatched > 300) {
+        for (let id in matchingParts) {
+            let axie = matchingParts[id];
+            let numD = 0;
+            for (let typ in typePartMap) {
+                if (selectedIds.includes(axie.traits[typ].d.partId)) {
+                    numD++;
+                }
+            }
+            //if you have a ton of axies with only recessives then this will not be good.
+            if (numD == 0) {
+                delete matchingParts[id];
+            }
+        }
+        let len = Object.keys(matchingParts).length;
+        console.log("Pruned: " + (numMatched - len) + ", total: " + len);
     }
     allProbs = [];
     let pairs = [];
