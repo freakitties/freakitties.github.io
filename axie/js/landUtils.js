1d4d36fa187268d5832b697b2aff5113b7112a9c
freak <freak>
2021-02-13 16:46:50 -0800

    updated map to use new apis. works with ronin now

--- axie/js/landUtils.js
+++ axie/js/landUtils.js
@@ -1,23 +1,67 @@
-async function getItems() {
-    let rarity = "";  //mystic, epic, rare, common, empty (land)
-    let sort = "lowest_price"; //lowest_price, recently_listed, highest_price
-    //let baseUrl = "https://axieinfinity.com/marketplace-api/query-assets?sorting=" + sort + "&rarity=" + rarity + "&offset=0&count=";
-    let baseUrl = "https://axieinfinity.com/marketplace-api/query-assets?sorting=" + sort + "&item_name=land&count=";
-    let r = await fetch(baseUrl + "1");
-    data = await r.json();
+async function getLandSales() {
+    let queryBody = "{\"operationName\":\"GetLandsGrid\",\"variables\":{\"from\":{OFFSET},\"size\":{SIZE},\"sort\":\"PriceAsc\",\"auctionType\":\"Sale\",\"owner\":null,\"criteria\":{\"landType\":[]}},\"query\":\"query GetLandsGrid($from: Int!, $size: Int!, $sort: SortBy!, $owner: String, $criteria: LandSearchCriteria, $auctionType: AuctionType) {\\n  lands(criteria: $criteria, from: $from, size: $size, sort: $sort, owner: $owner, auctionType: $auctionType) {\\n    total\\n    results {\\n      ...LandBriefV2\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment LandBriefV2 on LandPlot {\\n  tokenId\\n  owner\\n  landType\\n  row\\n  col\\n  auction {\\n    currentPrice\\n    startingTimestamp\\n    currentPriceUSD\\n    __typename\\n  }\\n  ownerProfile {\\n    name\\n    __typename\\n  }\\n  __typename\\n}\\n\"}";
+    //get count
+    let response = await (await fetch("https://axieinfinity.com/graphql-server-v2/graphql?r=freak", {
+      "headers": {
+        "content-type": "application/json",
+      },
+      "body": queryBody.replace("{OFFSET}", "0").replace("{SIZE}", "1"),
+      "method": "POST"
+    })).json();
 
-    let total = parseInt(data.total);
-    let url = baseUrl + total + "&offset=";
-    for (let offset = 0; offset < total; offset += 500) {
-        url = baseUrl + total + "&offset=" + offset;
-        r = await fetch(url);
-        data = await r.json();
-        items = data.results;
+    //get individual land sales
+    let lands = response.data.lands;
+    let total = parseInt(lands.total);
+    for (let offset = 0; offset < total; offset += 100) {
+        let response = await (await fetch("https://axieinfinity.com/graphql-server-v2/graphql?r=freak", {
+          "headers": {
+            "content-type": "application/json",
+          },
+          "body": queryBody.replace("{OFFSET}", offset).replace("{SIZE}", "100"),
+          "method": "POST"
+        })).json();
+
+        let items = response.data.lands.results;
         for (let i in items) {
             //if (items[i].assetType != "land") continue;
-            id = items[i].realTokenId;
+            id = items[i].tokenId;
             allItems[id] = items[i];
+            allItems[id].saleType = "single";
+        }
+    }
+
+    queryBody = "{\"operationName\":\"GetBundleList\",\"variables\":{\"from\":{OFFSET},\"size\":{SIZE},\"sort\":\"PriceAsc\",\"criteria\":{\"itemRarity\":[],\"landType\":[\"Genesis\",\"Arctic\",\"Savannah\",\"Mystic\",\"Forest\"],\"itemType\":[],\"totalItems\":[2,20],\"itemAlias\":[]}},\"query\":\"query GetBundleList($from: Int!, $size: Int!, $sort: SortBy, $seller: String, $criteria: BundleSearchCriteria) {\\n  bundles(from: $from, size: $size, criteria: $criteria, sort: $sort, seller: $seller) {\\n    total\\n    results {\\n      name\\n      listingIndex\\n      auction {\\n        currentPrice\\n        currentPriceUSD\\n        __typename\\n      }\\n      items {\\n        ... on LandItem {\\n          realTokenId\\n          figureURL\\n          rarity\\n          __typename\\n        }\\n        ... on LandPlot {\\n          realTokenId\\n          landType\\n          col\\n          row\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\"}";
+    //get bundles with lands
+    response = await (await fetch("https://axieinfinity.com/graphql-server-v2/graphql", {
+      "headers": {
+        "content-type": "application/json",
+      },
+      "body": queryBody.replace("{OFFSET}", "0").replace("{SIZE}", "1"),
+      "method": "POST",
+    })).json();
+
+    total = parseInt(response.data.bundles.total);
+    for (let offset = 0; offset < total; offset += 100) {
+        response = await (await fetch("https://axieinfinity.com/graphql-server-v2/graphql", {
+        "headers": {
+            "content-type": "application/json",
+        },
+        "body": queryBody.replace("{OFFSET}", offset).replace("{SIZE}", "100"),
+        "method": "POST",
+        })).json();
+
+        let bundles = response.data.bundles.results;
+        for (let i in bundles) {
+            for (let j in bundles[i].items) {
+                if (!bundles[i].items[j].hasOwnProperty("landType")) continue;
+                id = bundles[i].items[j].realTokenId;
+                allItems[id] = bundles[i].items[j];
+                allItems[id].auction = bundles[i].auction;
+                allItems[id].saleType = "bundle";
+            }
         }
 
     }
 }
+
+
